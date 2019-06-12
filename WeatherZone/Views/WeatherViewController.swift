59ae191b3d7ee7716b2f406d75729ca4c9d4697c
Nischal Hada <nischal@moroku.com>
2019-06-12 22:31:05 +1000

    Implemented weather search and display for weather API

---
+++ WeatherZone/Views/WeatherViewController.swift
@@ -0,0 +1,76 @@
+//
+//  WeatherViewController.swift
+//  WeatherZone
+//
+//  Created by Nischal Hada on 6/10/19.
+//  Copyright © 2019 NischalHada. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var cityNameTextField: UITextField!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.cityNameTextField.rx.controlEvent(.editingDidEndOnExit)
+            .asObservable()
+            .map { self.cityNameTextField.text }
+            .subscribe(onNext: { city in
+                
+                if let city = city {
+                    if city.isEmpty {
+                        self.displayWeather(nil)
+                    } else {
+                        self.fetchWeather(by: city)
+                    }
+                }
+                
+            }).disposed(by: disposeBag)
+        
+    }
+    
+    private func fetchWeather(by city: String) {
+        
+        guard let cityEncoded = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
+            let url = URL.urlForWeatherAPI(city: cityEncoded) else {
+                return
+        }
+        
+        let resource = Resource<WeatherResult>(url: url)
+        
+        let search = URLRequest.load(resource: resource)
+            .observeOn(MainScheduler.instance)
+            .asDriver(onErrorJustReturn: WeatherResult.empty)
+        
+        
+        search.map { "\($0.main.temp) ℉" }
+            .drive(self.temperatureLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        search.map { "\($0.main.humidity) 💦" }
+            .drive(self.humidityLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    private func displayWeather(_ weather: Weather?) {
+        
+        if let weather = weather {
+            self.temperatureLabel.text = "\(weather.temp) ℉"
+            self.humidityLabel.text = "\(weather.humidity) 💦"
+        } else {
+            self.temperatureLabel.text = "🙈"
+            self.humidityLabel.text = "⦰"
+        }
+    }
+    
+}
