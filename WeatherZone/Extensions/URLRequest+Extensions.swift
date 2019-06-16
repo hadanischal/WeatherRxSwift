1173f22ced6a2b02d08ddd405214a0779a3a1f3d
Nischal Hada <nischal@moroku.com>
2019-06-17 09:38:58 +1000

    Implemented error handelling

--- WeatherZone/Extensions/URLRequest+Extensions.swift
+++ WeatherZone/Extensions/URLRequest+Extensions.swift
@@ -16,6 +16,26 @@ struct Resource<T> {
 }
 
 extension URLRequest {
+    
+    static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
+        
+        return Observable.just(resource.url)
+            .flatMap { url -> Observable<(response: HTTPURLResponse, data: Data)> in
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.response(request: request)
+            }.map { response, data -> T in
+                
+                if 200..<300 ~= response.statusCode {
+                    return try JSONDecoder().decode(T.self, from: data)
+                } else {
+                    throw RxCocoaURLError.httpRequestFailed(response: response, data: data)
+                }
+                
+            }.asObservable()
+        
+    }
+    
+    /*
     static func load<T: Decodable>(resource: Resource<T>) -> Observable<T> {
         return Observable.from([resource.url])
             .flatMap { url -> Observable<Data> in
@@ -26,5 +46,5 @@ extension URLRequest {
             }.asObservable()
 
     }
-
+*/
 }
