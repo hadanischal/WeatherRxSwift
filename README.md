32df5204f7b976ecf44f0d7db617da47ca524ca2
Nischal Hada <nischal@moroku.com>
2019-06-20 11:43:48 +1000

    Implemented ViewModel and Api Handler

--- README.md
+++ README.md
@@ -8,6 +8,16 @@
 This demo is expected to be run using Swift 5.0 and Xcode 10.2.1
 
 ## Objective:
-This is a simple Demo project which aims to display weather information.
+This is a simple Demo project which aims to display weather information using MVVM pattern in Swift.
 * This project was intended to work as a  weather information demo projects for iOS using Swift. 
 * The demo uses the [Openweathermap API](http://api.openweathermap.org) as an excuse to have a nice use-case, because querying a WebService API is asynchronous by nature and is thus a good example for showing how It can be useful .
+* Use a UITableViewController to display weather information of Sydney, Melbourne and Brisbane as start.
+* Provide a way to add more cities using another modal view controller which includes a search functionality to find a city by name or location.
+* City IDs:
+* Sydney, Melbourne and Brisbane are: 4163971, 2147714, 2174003
+* More city can be found from  [Bulk Openweathermap API](http://bulk.openweathermap.org/sample/) 
+* Each cell should display at least two pieces of info: Name of city on the left, temperature on the right.
+* Get real time weather information using  [Openweathermap current API](https://openweathermap.org/current)  
+* A sample request to get weather info for one city: 
+* http://api.openweathermap.org/data/2.5/weather?id=4163971&units=metri c&APPID=your_registered_API_key
+* Weather should be automatically updated periodically.
--- WeatherZone/Views/WeatherViewController.swift
+++ WeatherZone/Views/WeatherViewController.swift
@@ -12,14 +12,25 @@ import RxCocoa
 
 class WeatherViewController: UIViewController {
     
+    private let disposeBag = DisposeBag()
+    var viewModel: WeatherViewModelProtocol = WeatherViewModel()
+    
     @IBOutlet weak var cityNameTextField: UITextField!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var humidityLabel: UILabel!
     
-    let disposeBag = DisposeBag()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.viewModel.weatherList
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] weather in
+                self?.displayWeather(weather)
+            }, onError: { error in
+                print("on error ")
+                print(error)
+            })
+        .disposed(by: disposeBag)
         
         self.cityNameTextField.rx.controlEvent(.editingDidEndOnExit)
             .asObservable()
@@ -30,45 +41,11 @@ class WeatherViewController: UIViewController {
                     if city.isEmpty {
                         self.displayWeather(nil)
                     } else {
-                        self.fetchWeather(by: city)
+                        self.viewModel.getWeatherInfo(by: city)
                     }
                 }
                 
             }).disposed(by: disposeBag)
-        
-    }
-    
-    private func fetchWeather(by city: String) {
-        
-        guard let cityEncoded = city.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed),
-            let url = URL.urlForWeatherAPI(city: cityEncoded) else {
-                return
-        }
-        
-        let resource = Resource<WeatherResult>(url: url)
-        
-//        let search = URLRequest.load(resource: resource)
-//            .observeOn(MainScheduler.instance)
-//            .asDriver(onErrorJustReturn: WeatherResult.empty)
-        
-        let search = URLRequest.load(resource: resource)
-            .observeOn(MainScheduler.instance)
-            .retry(3)
-            .catchError { error in
-                print(error.localizedDescription)
-                return Observable.just(WeatherResult.empty)
-            }.asDriver(onErrorJustReturn: WeatherResult.empty)
-        
-
-        
-        search.map { "\($0.main.temp) ℉" }
-            .drive(self.temperatureLabel.rx.text)
-            .disposed(by: disposeBag)
-        
-        search.map { "\($0.main.humidity) 💦" }
-            .drive(self.humidityLabel.rx.text)
-            .disposed(by: disposeBag)
-        
     }
     
     private func displayWeather(_ weather: Weather?) {
